798a48a9f686c44bc279e8eeccd2e803c766923f
Lekan <jimohridwanlekan@gmail.com>
2024-04-02 18:08:50 +0100

    Add total count cards

---
+++ src/pages/dashboard/components/deals-chart/index.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+import { useList } from "@refinedev/core";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+
+import { DollarOutlined } from "@ant-design/icons";
+import { Area, AreaConfig } from "@ant-design/plots";
+import { Card } from "antd";
+
+import { DashboardDealsChartQuery } from "@/graphql/types";
+
+import { DASHBOARD_DEALS_CHART_QUERY } from "./queries";
+import { mapDealsData } from "./utils";
+import { Text } from "@/components/text";
+
+export const DashboardDealsChart = () => {
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+    resource: "dealStages",
+    filters: [{ field: "title", operator: "in", value: ["WON", "LOST"] }],
+    meta: {
+      gqlQuery: DASHBOARD_DEALS_CHART_QUERY,
+    },
+  });
+
+  const dealData = React.useMemo(() => {
+    return mapDealsData(data?.data);
+  }, [data?.data]);
+
+  const config: AreaConfig = {
+    isStack: false,
+    data: dealData,
+    xField: "timeText",
+    yField: "value",
+    seriesField: "state",
+    animation: true,
+    startOnZero: false,
+    smooth: true,
+    legend: {
+      offsetY: -6,
+    },
+    yAxis: {
+      tickCount: 4,
+      label: {
+        formatter: (v: string) => {
+          return `$${Number(v) / 1000}k`;
+        },
+      },
+    },
+    tooltip: {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      formatter: (data: any) => {
+        return {
+          name: data.state,
+          value: `$${Number(data.value) / 1000}k`,
+        };
+      },
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    areaStyle: (datum: any) => {
+      const won = "l(270) 0:#ffffff 0.5:#b7eb8f 1:#52c41a";
+      const lost = "l(270) 0:#ffffff 0.5:#f3b7c2 1:#ff4d4f";
+      return { fill: datum.state === "Won" ? won : lost };
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    color: (datum: any) => {
+      return datum.state === "Won" ? "#52C41A" : "#F5222D";
+    },
+  };
+
+  return (
+    <Card
+      style={{ height: "100%" }}
+      headStyle={{ padding: "8px 16px" }}
+      bodyStyle={{ padding: "24px 24px 0px 24px" }}
+      title={
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
+          <DollarOutlined />
+          <Text size="sm" style={{ marginLeft: ".5rem" }}>
+            Deals
+          </Text>
+        </div>
+      }
+    >
+      <Area {...config} height={325} />
+    </Card>
+  );
+};
